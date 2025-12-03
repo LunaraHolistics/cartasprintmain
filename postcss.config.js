2cc3649b2173b75b36417e8bcdb446f3cc9167b7
LunaraHolistics <celsot.holistics@gmail.com>
2025-12-03 13:06:13 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
-import tailwindcss from 'tailwindcss';
-
-export default {
+// Usar module.exports é mais seguro para o PostCSS
+module.exports = {
   plugins: [
-    tailwindcss(), // Importa e executa o plugin do Tailwind
-    // O @tailwindcss/postcss não é necessário quando se importa a biblioteca diretamente
-    // e executa a função como plugin.
+    require('tailwindcss')(),
     require('autoprefixer'),
   ],
 };
