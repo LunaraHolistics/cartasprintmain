2c2a06191c0f45b22211c4ac6887b292c60802cc
LunaraHolistics <celsot.holistics@gmail.com>
2025-12-03 12:45:28 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    // CORREÇÃO: Usar o nome completo do pacote: @tailwindcss/postcss
+    '@tailwindcss/postcss': {}, 
     autoprefixer: {},
   },
 }
