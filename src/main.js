44798149bf5deed5db95a5bee3642b3668b67bea
LunaraHolistics <celsot.holistics@gmail.com>
2025-12-03 12:37:35 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './main.css'  // Note o ./ (significa: nesta mesma pasta)
-import App from './App.vue' // Note o ./ (significa: nesta mesma pasta)
+import './main.css'
+import App from './App.vue' 
 
 createApp(App).mount('#app')
