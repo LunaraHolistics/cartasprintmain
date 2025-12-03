18759954ec68bdf97f051cd66ad92e75904bb29c
LunaraHolistics <celsot.holistics@gmail.com>
2025-12-03 12:26:37 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './main.css' // Importando o CSS aqui (padrão Vite)
-import App from './App.vue' // Agora o App está na mesma pasta
+import './main.css'  // Note o ./ (significa: nesta mesma pasta)
+import App from './App.vue' // Note o ./ (significa: nesta mesma pasta)
 
 createApp(App).mount('#app')
