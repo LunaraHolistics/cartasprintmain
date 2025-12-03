93d56e457c7cdeb5254247b744886dc1a3483bb7
LunaraHolistics <celsot.holistics@gmail.com>
2025-12-03 13:18:27 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-// Usar module.exports é mais seguro para o PostCSS
+// Este padrão é o mais estável para ambientes de build como o Vercel.
 module.exports = {
   plugins: [
     require('tailwindcss')(),
     require('autoprefixer'),
   ],
-};
+}
